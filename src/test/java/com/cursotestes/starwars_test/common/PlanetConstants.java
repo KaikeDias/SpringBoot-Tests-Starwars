b5ba63f9176a2d5f7f6448c8b17be7ed403b3801
Kaike <kaikedias98@gmail.com>
2025-03-23 12:57:29 -0300

    feat: add planet constant

---
+++ src/test/java/com/cursotestes/starwars_test/common/PlanetConstants.java
@@ -0,0 +1,7 @@
+package com.cursotestes.starwars_test.common;
+
+import com.cursotestes.starwars_test.domain.Planet;
+
+public class PlanetConstants {
+    public static final Planet PLANET = new Planet("name", "climate", "terrain");
+}
