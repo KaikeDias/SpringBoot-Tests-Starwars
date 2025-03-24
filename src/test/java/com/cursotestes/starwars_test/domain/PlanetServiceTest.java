593ce9c581b6b151b01311270b56ad8ce501f6d7
Kaike <kaikedias98@gmail.com>
2025-03-24 15:13:12 -0300

    test: add tests to find planet by id

--- src/test/java/com/cursotestes/starwars_test/domain/PlanetServiceTest.java
+++ src/test/java/com/cursotestes/starwars_test/domain/PlanetServiceTest.java
@@ -1,7 +1,10 @@
 package com.cursotestes.starwars_test.domain;
 
 
+import static com.cursotestes.starwars_test.common.PlanetConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,8 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import static com.cursotestes.starwars_test.common.PlanetConstants.PLANET;
-import static com.cursotestes.starwars_test.common.PlanetConstants.INVALID_PLANET;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
@@ -41,5 +44,24 @@ public class PlanetServiceTest {
         when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
 
         assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() {
+        when(planetRepository.findById(1L)).thenReturn(Optional.of(PLANET));
+
+        Optional<Planet> sut = planetService.findById(1L);
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void getPlanet_ByUnexistentId_ReturnsEmpty() {
+        when(planetRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Optional<Planet> sut = planetService.findById(1L);
+
+        assertThat(sut).isEmpty();
     }
 }
