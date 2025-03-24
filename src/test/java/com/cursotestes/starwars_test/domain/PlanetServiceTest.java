2992ba46f9bc4155d44d06ebfeedb918ff715320
Kaike <kaikedias98@gmail.com>
2025-03-24 13:49:05 -0300

    feat: add test for createPlanet

---
+++ src/test/java/com/cursotestes/starwars_test/domain/PlanetServiceTest.java
@@ -0,0 +1,36 @@
+package com.cursotestes.starwars_test.domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static com.cursotestes.starwars_test.common.PlanetConstants.PLANET;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+//@SpringBootTest(classes = PlanetService.class)
+public class PlanetServiceTest {
+    //@Autowired
+    @InjectMocks
+    private PlanetService planetService;
+
+    //@MockitoBean
+    @Mock
+    private PlanetRepository planetRepository;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet() {
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
+        Planet sut = planetService.create(PLANET);
+
+        assertThat(sut).isEqualTo(PLANET);
+    }
+}
